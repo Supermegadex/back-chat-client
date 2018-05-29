c924fce470db198be9dfdeeebbf335e500959366
Daniel Noon <supercookies@outlook.com>
2018-05-28 20:05:03 -0500

    Get rid of list for prod

--- src/pages/channel/channel.ts
+++ src/pages/channel/channel.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavParams } from 'ionic-angular';
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
 
